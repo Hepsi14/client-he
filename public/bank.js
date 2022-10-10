8eeaa682de5e0fba4a7103976184c06eb25cb433
Hepsipriyanka S <91179177+Hepsi14@users.noreply.github.com>
2022-10-10 10:51:19 +0530

    Add files via upload

---
+++ public/bank.js
@@ -0,0 +1,16 @@
+
+  
+  
+import "./App.css";
+//  import bank from "./image/bank.jpg"
+export default function Bank(){
+	
+	return (<><center>
+	
+	<h1 className="navi">SWISS NATIONAL BANK</h1>
+	{/* <img  className="img"src={bank} width="40" height="40"/> */}
+    <img src='https://cdn.dribbble.com/users/2243452/screenshots/6339193/qcash.gif'   width="70%"   height="70%"/>
+	</center></>)
+}
+
+
